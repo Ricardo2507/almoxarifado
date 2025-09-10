f7b9391f2706513f432d25c2866da77331e6aaf0
Ricardo Sérgio Carvalho <rscarvalho@hotmail.com>
2025-09-10 15:40:46 -0300

    terceiro commit

--- almoxarifado/settings.py
+++ almoxarifado/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wx5n%dvs3&9@gq)o8d=j&i9umrfvykvvc$t13y913odn4x8rot'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
