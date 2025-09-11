38de84eefc93658c8217a0d81becb5e9f1c3fde0
Ricardo Sérgio Carvalho <rscarvalho@hotmail.com>
2025-09-11 14:21:47 -0300

    oitivao commit

--- almoxarifado/settings.py
+++ almoxarifado/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 #STATIC_URL = '/static/'
-STATIC_URL = 'https://cdn.jsdelivr.net/npm/django@4.2.10/contrib/admin/static/admin/'
+STATIC_URL = 'https://cdn.jsdelivr.net/npm/django@5.2.6/contrib/admin/static/admin/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
